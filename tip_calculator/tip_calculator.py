69b04196af361ac4da910e4793c4902877628d7e
AnthonyAkiniz <70271226+AnthonyAkiniz@users.noreply.github.com>
2022-06-20 00:45:35 -0500

    Add files via upload

---
+++ tip_calculator/tip_calculator.py
@@ -0,0 +1,29 @@
+#########################################################################################
+# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * #
+# * ################################################################################# * #
+# * #                               Tip Calculator                                  # * #
+# * #                          project by: Anthony Akiniz                           # * #
+# * #                          github.com/anthonyakiniz                             # * #
+# * ################################################################################# * #
+# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * #
+#########################################################################################
+# Info:                                                                                 #
+# Calculator that helps you split a check with the tip included.                        #
+#                                                                                       #
+# Guide:                                                                                #
+# cd into project folder: cd tip_calculator                                             #
+# Launch by clicking run button in top right in VSCode or: py -3 main.py                #
+#########################################################################################
+
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill?"))
+
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
+
+print(f"Each person should pay: ${final_amount}")
---
+++ trivia_api_app/main.py
@@ -0,0 +1,33 @@
+#########################################################################################
+# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * #
+# * ################################################################################# * #
+# * #                                Trivia API App                                 # * #
+# * #                          project by: Anthony Akiniz                           # * #
+# * #                          github.com/anthonyakiniz                             # * #
+# * ################################################################################# * #
+# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * #
+#########################################################################################
+# Info:                                                                                 #
+# Quiz questions come from Open Trivia Database API.                                    #
+#                                                                                       #
+# Guide:                                                                                #
+# Uses general Open Trivia Database API URL, no personal API key required.              #
+# cd into project folder: cd trivia_api_app                                             #
+# Launch by clicking run button in top right in VSCode or: py -3 main.py                #
+#########################################################################################
+
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+from ui import QuizInterface
+
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface(quiz)
